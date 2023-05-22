413b9937a9416e52104550cceea11c84abdc8324
Mohit Kamdi <130160983+Mohit27071998@users.noreply.github.com>
2023-05-22 15:32:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function volume_sphere() {
     //Write your code here
+	let rad =document.getElementById("radius").value
+
+	let ans = (4/3) * 3.14 * (rad*rad*rad);
+
+	let vol =document.getElementById("volume")
+	vol.value = ans;
+	return false;
   
 } 
 
